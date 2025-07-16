469e18ae6791cd4c945e8da185e1a578e5bcd713
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 16:10:30 -0500

    💄 Translate login to spanish. Add some styles to button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,14 +9,14 @@ export default function Login() {
   return (
     <div className="grid grid-cols-1 justify-center items-center h-dvh w-full">
       <div className="flex flex-col justify-center align-middle w-8/9 max-w-120 mx-auto backdrop-blur-lg bg-gray-400/10 shadow shadow-gray-400/60 sm:px-15 px-6 pt-12 pb-8 rounded-xl">
-        <h1 className="text-center text-3xl">Hello, login here</h1>
+        <h1 className="text-center text-3xl">Hola, inicia sesión</h1>
 
         <form action={action} className="flex flex-col mt-9 mb-6">
-          <label htmlFor="username" className="text-xl">Username</label>
+          <label htmlFor="username" className="text-xl">Usuario</label>
           <input type="text" name="username" id="username" className="bg-gray-300/20 rounded-xl p-2 my-3" />
           { state?.errors?.username && <div className="bg-red-500/50 p-5 mb-3 rounded-sm"><p>{ state.errors.username }</p></div> }
 
-          <label htmlFor="password" className="text-xl">Password</label>
+          <label htmlFor="password" className="text-xl">Contraseña</label>
           <input type="password" name="password" id="password" className="bg-gray-300/20 rounded-xl p-2 my-3" />
           {state?.errors?.password && (
             <div className="bg-red-500/50 p-5 mb-3 rounded-sm">
@@ -29,8 +29,8 @@ export default function Login() {
             </div>
           )}
 
-          <button disabled={pending} className="mx-auto rounded-xl bg-blue-600 hover:bg-blue-400 hover:cursor-pointer text-xl mt-6 py-2 px-6 w-11/12">
-            Login
+          <button disabled={pending} className="mx-auto rounded-xl bg-blue-600 hover:bg-blue-400 disabled:bg-blue-300 hover:cursor-pointer disabled:cursor-not-allowed text-xl mt-6 py-2 px-6 w-11/12">
+            Entrar
           </button>
         </form>
       </div>

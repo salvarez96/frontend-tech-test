8065359b696c14f7712dc0316636cb4827cc2b88
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 18:06:18 -0500

    🛂 Implement a unique and valid username and password
    Lock login to a specific username and password

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -4,6 +4,9 @@ import { LoginFormSchema, LoginFormState } from "@/lib/definitions";
 import { Session } from "@/lib/Session";
 import { redirect } from "next/navigation";
 
+const VALID_USERNAME = process.env.VALID_USERNAME
+const VALID_PASSWORD = process.env.VALID_PASSWORD
+
 export async function login(state: LoginFormState, formData: FormData) {
   const validatedFields = LoginFormSchema.safeParse({
     username: formData.get('username'),
@@ -18,7 +21,17 @@ export async function login(state: LoginFormState, formData: FormData) {
     }
   }
 
-  const { username } = validatedFields.data
+  const { username, password } = validatedFields.data
+
+  if (username !== VALID_USERNAME && password !== VALID_PASSWORD) {
+    return {
+      success: false,
+      message: 'Invalid username and password',
+      errors: {
+        username: ['Usuario y contraseña inválidos'],
+      }
+    }
+  }
 
   await Session.createSession(username)
 

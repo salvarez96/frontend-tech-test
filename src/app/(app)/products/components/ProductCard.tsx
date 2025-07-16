e2534d179f4982e4e6d12b6d3378bd40a9b3320a
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 01:50:13 -0500

    💄 Create and implement product card

---
+++ src/app/(app)/products/components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import { Product } from "@/services/FakeApiService";
+import Image from "next/image";
+import Link from "next/link";
+import EditIcon from '@mui/icons-material/Edit';
+
+export function ProductCard({ product }: { product: Product }) {
+  return (
+    <div className="flex flex-col justify-center align-middle w-8/9 max-w-120 mx-auto backdrop-blur-lg bg-gray-400/10 shadow shadow-gray-400/60 sm:px-8 px-5 py-7 rounded-xl">
+      <Link href={`/products/${product.id}`} className="text-end hover:text-blue-400" title="Edit"><EditIcon /></Link>
+      <div className="flex flex-col md:flex-row gap-5 my-5">
+        <Image
+          src={ product.image }
+          alt={ product.title + ' image' }
+          width={ 120 }
+          height={ 120 }
+          className="mx-auto lg:mx-0 rounded-md"
+        />
+        <div className="flex flex-col">
+          <p className="text-xl">{ product.title }</p>
+          <p className="text-sm">{ product.category }</p>
+          <p className="text-2xl self-end">${ product.price }</p>
+        </div>
+      </div>
+      <p className="line-clamp-4 overflow-hidden">{ product.description }</p>
+    </div>
+  )
+}
--- src/app/(app)/products/page.tsx
+++ src/app/(app)/products/page.tsx
@@ -1,7 +1,16 @@
-export default function Products() {
+import { FakeApiService } from "@/services/FakeApiService";
+import { ProductCard } from "./components/ProductCard";
+
+export default async function Products() {
+  const products = await FakeApiService.getProducts()
   return (
     <>
-      <h1>Products page</h1>
+      <h1 className="ml-5 text-4xl">{products.length} products found</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10 mb-20">
+        { products.map((product, index) => (
+          <ProductCard product={ product } key={ product.title + index } />
+        ))}
+      </div>
     </>
   )
 }

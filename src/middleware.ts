2da726cec8268e1787832489d8d54ab7c85c455f
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-15 20:04:57 -0500

    🛂 Create middleware to protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { decrypt } from '@/lib/session'
+
+const protectedRoutes = ['/products']
+const publicRoutes = ['/login', '/']
+const redirectTo = (req: NextRequest, pathname: string) => NextResponse.redirect(new URL(pathname, req.nextUrl))
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.some(route => path.startsWith(route))
+  const isPublicRoute = publicRoutes.some(route => path.startsWith(route))
+
+  const cookie = req.cookies.get('session')?.value
+  const session = await decrypt(cookie)
+
+  if (isProtectedRoute && !session?.username) {
+    return redirectTo(req, '/login')
+  }
+
+  if (req.nextUrl.pathname === '/') {
+    return redirectTo(req, session?.username ? '/products' : '/login')
+  }
+
+  if (
+    isPublicRoute &&
+    session?.username &&
+    !req.nextUrl.pathname.startsWith('/products')
+  ) {
+    return redirectTo(req, '/products')
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}

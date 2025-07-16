f078af277ce6123d1e2a342ea21d3cddc639094c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 01:51:04 -0500

    💄 Use logout icon for logout button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { logout } from "@/app/actions/auth";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export function LogoutButton() {
   return (
     <form className="flex align-center" action={logout}>
-      <button type="submit" className="hover:cursor-pointer hover:text-red-400">Cerrar sesión</button>
+      <button type="submit" className="hover:cursor-pointer hover:text-red-400"><LogoutIcon /> Salir</button>
     </form>
   )
 }

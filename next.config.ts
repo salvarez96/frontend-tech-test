e05aa1c78d2de775755a1dbc9bdf7300b94b0909
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 01:49:25 -0500

    🔧 Add fakeapi url to next's remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL((process.env.NEXT_PUBLIC_FAKE_STORE_API as string) + '/**')]
+  }
 };
 
 export default nextConfig;

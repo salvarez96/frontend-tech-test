be35a9d2c0251693a8dcadd3aa08e93a36161e53
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 20:20:55 -0500

    🔧 Add jest and babel config files

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  coverageProvider: 'babel',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "@/(.*)$": ["<rootDir>/src/$1"],
+    "@/services/(.*)$": ["<rootDir>/src/services/$1"],
+    "@/helpers/(.*)$": ["<rootDir>/src/helpers/$1"],
+    "@/components/(.*)$": ["<rootDir>/src/components/$1"],
+    "@/lib/(.*)$": ["<rootDir>/src/lib/$1"]
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(jose)/)',
+  ],
+}
+
+export default createJestConfig(config)

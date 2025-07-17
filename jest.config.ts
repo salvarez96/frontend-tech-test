6b91f6cc970188db0d50e067731a56ae698965a8
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2025-07-16 20:33:29 -0500

    ➖ Uninstall and remove babel due to conflicts

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 })
 
 const config: Config = {
-  coverageProvider: 'babel',
+  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     "@/(.*)$": ["<rootDir>/src/$1"],
